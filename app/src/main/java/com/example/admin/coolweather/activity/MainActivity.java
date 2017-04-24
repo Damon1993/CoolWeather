42c343b83a36d322037069fd2129084d471be891
Damon <w1604577061@sina.com>
2017-04-24 16:31:46 +0800

    完成遍历省市县三级列表功能

--- app/src/main/java/com/example/admin/coolweather/activity/MainActivity.java
+++ app/src/main/java/com/example/admin/coolweather/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.admin.coolweather;
+package com.example.admin.coolweather.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.admin.coolweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/admin/coolweather/util/HttpUtil.java
@@ -0,0 +1,22 @@
+package com.example.admin.coolweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by Administrator on 2017/4/24.
+ */
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback){
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+
+
+    }
+
+
+
+}
